64415a99e60f68c5f53ac31f7ea689290888ca20
MorvenMac <mmacle218@caledonian.ac.uk>
2025-01-08 00:55:10 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,14 @@ const port = process.env.PORT || 4000;
 const app = express();
 // Allow requests from the frontend Render URL
 const corsOptions = {
-  origin: ['https://web-conference-1.onrender.com'], // Replace with your frontend Render URL
+  origin: [
+    'https://web-conference-1.onrender.com', // Your frontend Render URL
+    'http://localhost:3000',                // Local React URL for development
+  ],
   credentials: true,
 };
-
 app.use(cors(corsOptions));
+
 //app.use(cors());
 // app.use(
 //   cors({
